be93b38f50de8aacf3afe1d751df131b66be3b05
Andrew Davis <spinup@gmail.com>
2020-10-24 15:10:16 -0400

    expose plot_psd in main heartpy module

--- heartpy/heartpy.py
+++ heartpy/heartpy.py
@@ -21,7 +21,8 @@ from .filtering import filter_signal, hampel_filter, hampel_correcter, \
                        remove_baseline_wander, smooth_signal
 from .peakdetection import make_windows, append_dict, fit_peaks, check_peaks, \
                            check_binary_quality, interpolate_peaks
-from .visualizeutils import plotter, segment_plotter, plot_poincare, plot_breathing, report
+from .visualizeutils import plotter, segment_plotter, plot_poincare, plot_breathing, \
+                            plot_psd, report
 from .analysis import calc_rr, calc_rr_segment, clean_rr_intervals, calc_ts_measures, \
                       calc_fd_measures, calc_breathing, calc_poincare
 
@@ -38,6 +39,7 @@ __all__ = ['enhance_peaks',
            'load_exampledata',
            'plotter',
            'plot_breathing',
+           'plot_psd',
            'report',
            'plot_poincare',
            'process',
